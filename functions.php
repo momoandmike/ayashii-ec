c65819334c75a17a74daf592ba10d7895cdeecea
momoandmike <momoandmike@gmail.com>
2022-03-12 10:04:35 +0900

    土曜日の朝

--- functions.php
+++ functions.php
@@ -20,5 +20,59 @@ function connect_db()
 
 function h($str)
 {
+    // ENT_QUOTES: シングルクオートとダブルクオートを共に変換する。
     return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+}
+
+// タスク登録
+function insert_task($title)
+{
+    // データベースに接続
+    $dbh = connect_db();
+
+    // レコードを追加
+    $sql = <<<EOM
+    INSERT INTO
+        tasks
+        (title)
+    VALUES
+        (:title)
+    EOM;
+
+    // プリペアドステートメントの準備
+    $stmt = $dbh->prepare($sql);
+
+    // パラメータのバインド
+    $stmt->bindParam(':title', $title, PDO::PARAM_STR);
+
+    // プリペアドステートメントの実行
+    $stmt->execute();
+}
+// status に応じてレコードを取得
+function find_task_by_status($status)
+{
+// データベースに接続
+    $dbh = connect_db();
+
+    // status で該当レコードを取得
+    $sql = <<<EOM
+    SELECT
+    * 
+    FROM 
+        tasks
+    WHERE 
+        status = :status;
+    EOM;
+
+    // プリペアドステートメントの準備
+    $stmt = $dbh->prepare($sql);
+
+    // パラメータのバインド
+    $stmt->bindParam(':status', $status, PDO::PARAM_STR);
+
+      // プリペアドステートメントの実行
+    $stmt->execute();
+
+      // 結果の取得
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
--- index.php
+++ index.php
@@ -1,5 +1,44 @@
 <?php
 
 require_once __DIR__ . '/functions.php';
+require_once __DIR__ . '/config.php';
 
 $dbh = connect_db();
+
+/* タスク登録---------------------------------------------*/
+// 初期化
+$title = '';
+
+// リクエストメソッドの判定
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // フォームに入力されたデータを受け取る
+    $title = filter_input(INPUT_POST, 'title');
+
+    // タスク登録処理の実行
+    // 後程 ここに insert_task関数を呼び出す処理を追記する
+insert_task($title);
+}
+
+/* タスク照会
+---------------------------------------------*/
+// 未完了タスクの取得
+$notyet_tasks = find_task_by_status(TASK_STATUS_NOTYET);
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+
+// status を抽出条件に指定してデータを取得
+$sql = <<<EOM SELECT * FROM ayashii_db WHERE //status=:status; EOM; // プリペアドステートメントの準備 $stmt=$dbh->prepare($sql);
+
+    // バインド(代入)するパラメータの準備
+    $status = 'notyet';
+
+    // パラメータのバインド
+    $stmt->bindParam(':status', $status, PDO::PARAM_STR);
+
+    // プリペアドステートメントの実行
+    $stmt->execute();
+
+    // 結果の取得
+    $notyet_tasks = $stmt->fetchAll(PDO::FETCH_ASSOC);
